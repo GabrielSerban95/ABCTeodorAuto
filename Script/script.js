5e4092bce1a3a54c2d4c4d71769864f7bd8a1583
GabrielSerban95 <124520766+GabrielSerban95@users.noreply.github.com>
2024-01-29 22:42:18 +0200

    dif changes

--- Script/script.js
+++ Script/script.js
@@ -1,64 +1,49 @@
 $(document).ready(function () {
-
-
-
-
-
-
-
-
-
     //////////////////////
     /// Scroll fadeIn  ///
-    /// and fadeOut at ///
-    ///     100px      ///
+    /// and fadeOut    ///
     //////////////////////
 
     $(window).scroll(function () {
         // Ajustează înălțimea minimă a header-ului în funcție de poziția de scroll
-        $("header").css("min-height", ($(window).scrollTop() > 50) ? 150 : 300);
+        if ($(window).width() < 768) {
+            $("header").css("min-height", ($(window).scrollTop() > 50) ? 150 : 200);
 
-        // Definirea elementelor și a condițiilor specifice pentru fiecare
-        var scrollSections = [
-            { selector: ".page1", start: 0, end: 3 },
-            { selector: ".page2", start: 3, end: 6 },
-            { selector: ".page3", start: 6, end: 9 },
-            { selector: ".page4", start: 9, end: Infinity } // Infinity pentru a acoperi cazul când scroll-ul depășește 9
-        ];
+            // Definirea elementelor și a condițiilor specifice pentru fiecare
+            var scrollSections = [
+                { selector: ".page1", start: 0, end: 18 },
+                { selector: ".page2", start: 18, end: 36 },
+                { selector: ".page3", start: 36, end: 54 },
+                { selector: ".page4", start: 54, end: Infinity } // Infinity pentru a acoperi cazul când scroll-ul depășește 54
+            ];
+        }
+        else {
+            $("header").css("min-height", ($(window).scrollTop() > 50) ? 150 : 300);
+            var scrollSections = [
+                { selector: ".page1", start: 0, end: 3 },
+                { selector: ".page2", start: 3, end: 6 },
+                { selector: ".page3", start: 6, end: 9 },
+                { selector: ".page4", start: 9, end: Infinity } // Infinity pentru a acoperi cazul când scroll-ul depășește 9
+            ];
+        }
 
         var distanceFromTop = $(window).scrollTop();
         // Iterează prin fiecare secțiune și setează opacitatea corespunzător
         for (var i = 0; i < scrollSections.length; i++) {
             var section = scrollSections[i];
             var opacity = (distanceFromTop / 100 >= section.start && distanceFromTop / 100 <= section.end) ? 1 : 0;
+            var translateX = (distanceFromTop / 100 >= section.start && distanceFromTop / 100 < section.end) ? 1 : 0;
             $(section.selector).css("opacity", opacity);
+
+            // console.log((parseInt(distanceFromTop / 72) / 4) + "%");
+            if (translateX == 1) {
+                $(section.selector).css("transform", "translate(" + 0 + ")");
+            }
+            else {
+                $(section.selector).css("transform", "translate(" + -100 + "%)");
+            }
         }
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 
 
@@ -86,11 +71,5 @@ $(document).ready(function () {
     $('#instagram').mouseleave(function () {
         $(this).removeClass('fa-bounce');
     });
-
-
-
-
-
-
 
 })
